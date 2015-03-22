b4f19eba1066224ede8e3f71f2ef9beaa187d008
Yu Shi <mynsa04@ya.ru>
2015-03-23 01:54:55 +0700

    - man

--- man/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Money\\AvailableCurrencies"],["c","Money\\CurrenciesProperties"],["c","Money\\Currency"],["c","Money\\CurrencyExchanger"],["c","Money\\CurrencyPair"],["c","Money\\ISOCurrencies"],["c","Money\\Money"],["c","Money\\UnknownCurrencyException"]];
